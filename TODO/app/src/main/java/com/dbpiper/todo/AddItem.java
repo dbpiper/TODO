d1159d53662989688e0daa2ee0294ddeeb0c6ce1
David Piper <dbpiper@gmail.com>
2017-04-03 13:36:46 -0500

    Change Add Item Screen to more closely reflect ui-spec by adding: back button, edit text on toolbar, and edittext in body.

--- TODO/app/src/main/java/com/dbpiper/todo/AddItem.java
+++ TODO/app/src/main/java/com/dbpiper/todo/AddItem.java
@@ -2,6 +2,7 @@ package com.dbpiper.todo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class AddItem extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class AddItem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+        Toolbar toolbarAddItem = (Toolbar) findViewById(R.id.toolbarAddItem);
+        setSupportActionBar(toolbarAddItem);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
